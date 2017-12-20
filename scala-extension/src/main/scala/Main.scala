3d3b083a33f10b6f6eb40a4fedf29b7321d8c135
domoszlai <dlacko@gmail.com>
2017-12-20 23:33:18 +0100

    add experiment with implicit class

--- scala-extension/src/main/scala/Main.scala
+++ scala-extension/src/main/scala/Main.scala
@@ -1,10 +1,21 @@
 import javafx.application.Application;
 
+import footprint.layout.Point;
 import footprint.layout.Layout;
 import footprint.layout.Layouter;
 import footprint.layout.Line;
 import footprint.layout.Pad;
 import footprint.viewer.Viewer;
+
+import Extension._
+
+object Extension {
+   implicit class PointExtension(p: Point)
+   {
+		val x = p.getX();
+		val y = p.getY();
+   }
+}
 
 class Main extends Viewer {
 
@@ -15,6 +26,8 @@ class Main extends Viewer {
 	 val p = l.createPoint("P"); 
 	 p.addConstraint(0, 0);
       
+	 System.out.println(p.x); 
+	  
      var layout = l.generate();
      System.out.println(layout);
      return layout;     
