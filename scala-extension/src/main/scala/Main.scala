4b35881ac537b0417283c5c79f5e4b7caa9c1d90
domoszlai <dlacko@gmail.com>
2017-12-20 21:48:50 +0100

    create scala sample to work with

--- scala-extension/src/main/scala/Main.scala
+++ scala-extension/src/main/scala/Main.scala
@@ -1,5 +1,30 @@
+import javafx.application.Application;
+
+import footprint.layout.Layout;
+import footprint.layout.Layouter;
+import footprint.layout.Line;
+import footprint.layout.Pad;
+import footprint.viewer.Viewer;
+
+class Main extends Viewer {
+
+  def onLayout():Layout = {
+   
+     val l = new Layouter();
+	 
+	 val p = l.createPoint("P"); 
+	 p.addConstraint(0, 0);
+      
+     var layout = l.generate();
+     System.out.println(layout);
+     return layout;     
+   }
+
+}
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    Application.launch(classOf[Main], args: _*)
   }
 }
+ 
